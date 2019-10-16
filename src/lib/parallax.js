297850cdba2f873eaabd9684e5c71369be69aae8
moniet <moniet.sawhney@gmail.com>
2019-10-16 09:16:04 +0530

    added reverse parallax

--- src/lib/parallax.js
+++ src/lib/parallax.js
@@ -1,15 +1,25 @@
-export const parallax = (bg, el, q = 40) => {
-    let obj = el.getBoundingClientRect()
+export const parallax = (bg, el, q = 40, rev = false) => {
+	let obj = el.getBoundingClientRect()
 
-    bg.addEventListener('mousemove', e => {
-        let translateX = ( e.clientX - (obj.left + (obj.width / 2)) ) / q
-        let translateY = ( e.clientY - (obj.top + (obj.height / 2)) ) / q
-        el.style.transform = `translate(${Math.floor(translateX)}px, ${Math.floor(translateY)}px)`
-    });
+	bg.addEventListener('mousemove', e => {
+		let translateX
+		let translateY
 
-    bg.addEventListener(
-      "mouseleave",
-      () => (el.style.transform = 'translateX(0)')
-    );
+		if (rev) {
+			translateX = (obj.left + obj.width / 2 - e.clientX) / q
+			translateY = (obj.top + obj.height / 2 - e.clientY) / q
+		} else {
+			translateX = (e.clientX - (obj.left + obj.width / 2)) / q
+			translateY = (e.clientY - (obj.top + obj.height / 2)) / q
+		}
+
+		el.style.transform = `translate(${Math.floor(translateX)}px, ${Math.floor(
+			translateY
+		)}px)`
+	})
+
+	bg.addEventListener(
+		'mouseleave',
+		() => (el.style.transform = 'translateX(0)')
+	)
 }
-
